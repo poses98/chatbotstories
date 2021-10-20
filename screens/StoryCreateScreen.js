9439b1802798678c2afc7c1074b6c96fd7e5ab42
Pablo Osés <poses98@MacBook-Pro-de-Pablo.local>
2021-10-20 09:41:08 +0200

    Interactivity is now changed in the database when story is updated or created

--- screens/StoryCreateScreen.js
+++ screens/StoryCreateScreen.js
@@ -60,6 +60,7 @@ export default ({ route, navigation }) => {
     text: "",
   });
   const [interactive, setinteractive] = useState(false); // interactivity of the story
+  const [oldInteractive, setOldInteractive] = useState(false); //check variable for old interactivity
   const [status, setstatus] = useState(0); // status of the story
   const [language, setlanguage] = useState(0); // language of the story
 
@@ -90,6 +91,7 @@ export default ({ route, navigation }) => {
               setcategoryMain(doc.data().categoryMain);
               setOldCategory(doc.data().categoryMain);
               setinteractive(doc.data().interactive);
+              setOldInteractive(doc.data().interactive);
               setlanguage(doc.data().language);
             } else {
               setStoryId("");
@@ -108,7 +110,10 @@ export default ({ route, navigation }) => {
       setloading(false);
     }
   }, []);
-
+  /**Creates a new story from the data written by the user in the form
+   *
+   * @param {form data}} data
+   */
   const createStory = (data) => {
     firestore()
       .collection("stories")
@@ -132,6 +137,14 @@ export default ({ route, navigation }) => {
           .collection("stories")
           .doc(docRef.id)
           .set({ id: false });
+        //if story is intereactive adding it to the interactive category
+        if (interactive) {
+          categoryRef
+            .doc("i")
+            .collection("stories")
+            .doc(docRef.id)
+            .set({ id: false });
+        }
         console.log("Document written with ID: ", docRef.id);
         //Creating reference to the story in user's collection
         firestore()
@@ -156,15 +169,19 @@ export default ({ route, navigation }) => {
         console.error("Error adding document: ", error);
       });
   };
-
+  /**Updates an existing story with the new data provided in the form
+   *
+   * @param {form data} data
+   */
   const updateStory = (data) => {
     data.storyId = storyId;
     setdata({ ...data });
     updateDoc(storyRef, storyId, data);
-    console.log("old category:",oldCategory)
-    console.log("new category:",categoryMain)
+    console.log("old category:", oldCategory);
+    console.log("new category:", categoryMain);
+    //checking if the category of the story has changed
     if (oldCategory != data.categoryMain) {
-      console.log("proceeding to delete old category")
+      console.log("proceeding to delete old category");
       const categoryRef = firestore().collection("storyCategories");
       //Adding story to the respecting category
       categoryRef
@@ -172,11 +189,36 @@ export default ({ route, navigation }) => {
         .collection("stories")
         .doc(storyId)
         .set({ id: false });
-        categoryRef
+      categoryRef
         .doc(`${oldCategory}`)
         .collection("stories")
         .doc(storyId)
         .delete();
+    }
+    //checking if the interactivity of the story has changed
+    if (oldInteractive != data.interactive) {
+      console.log(
+        "interactivity has changed, proceeding to make changes on the db"
+      );
+      const categoryRef = firestore().collection("storyCategories");
+      //if the new interactivity is true it means that we just need to add
+      //the story to the interactive category ("i"). Otherwise we need to delete
+      //it from that same category.
+      if (data.interactive) {
+        categoryRef
+          .doc("i")
+          .collection("stories")
+          .doc(storyId)
+          .set({ id: false });
+      } else if (!data.interactive) {
+        categoryRef
+          .doc("i")
+          .collection("stories")
+          .doc(storyId)
+          .delete();
+      } else {
+        console.log("Unexpected input.");
+      }
     }
     //Creating reference to the story in user's collection
     firestore()
@@ -197,7 +239,7 @@ export default ({ route, navigation }) => {
         console.error("Error adding document: ", error);
       });
   };
-
+  /** GENRE BUBBLE TEMPLATE */
   const GenreBubble = ({ image, verboseName, genreKey }) => {
     return (
       <View style={{ flex: 1, flexDirection: "row", marginHorizontal: 10 }}>
@@ -225,6 +267,7 @@ export default ({ route, navigation }) => {
     <ScrollView style={styles.container}>
       {!loading && !notloaded && (
         <View>
+          {/** TITLE FIELD */}
           <LabeledInput
             label="Name"
             text={nameField.text}
@@ -271,7 +314,7 @@ export default ({ route, navigation }) => {
               );
             }}
           />
-
+          {/** INTERACTIVE SWITCH */}
           <View style={{ flexDirection: "row", marginTop: 15 }}>
             <Label text="Will it be interactive?" icon="people-outline" />
             <Switch
@@ -285,7 +328,7 @@ export default ({ route, navigation }) => {
               style={{ marginRight: 15 }}
             />
           </View>
-
+          {/** PICKER FOR STATUS */}
           <View style={{ flexDirection: "column", marginTop: 15 }}>
             <Label text="Story status" icon="list-outline" />
             <Picker
@@ -310,7 +353,7 @@ export default ({ route, navigation }) => {
               />
             </Picker>
           </View>
-
+          {/** PICKER FOR LANGUAGE */}
           <View style={{ flexDirection: "column", marginTop: 15 }}>
             <Label text="Language" icon="list-outline" />
             <Picker
@@ -354,7 +397,10 @@ export default ({ route, navigation }) => {
               if (validation) {
                 const data = {
                   title: nameField.text,
-                  description: descriptionField.text.replace(/(\r\n|\n|\r)/gm, ""),
+                  description: descriptionField.text.replace(
+                    /(\r\n|\n|\r)/gm,
+                    ""
+                  ),
                   categoryMain: categoryMain,
                   date: Date.now(),
                   author: auth().currentUser.uid,
