4ae31d340971917928fda17e29ff4f2ae49d8ae5
Pablo Osés <posesand@gmail.com>
2023-06-03 11:45:44 +0200

    fetching liked,saved and read stories in story hook

--- providers/StoryProvider.js
+++ providers/StoryProvider.js
@@ -7,18 +7,33 @@ export const StoryContext = createContext();
 
 const StoryProvider = ({ children }) => {
   const [userStories, setUserStories] = useState(null);
+  const [likedStories, setLikedStories] = useState(null);
+  const [savedStories, setSavedStories] = useState(null);
+  const [readStories, setReadStories] = useState(null);
   const { authUser } = useAuth();
 
   useEffect(() => {
-    if (authUser) {
-      StoryApi.getUserStories(authUser._id)
-        .then((response) => {
-          setUserStories(response);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
+    const fetchData = async () => {
+      try {
+        if (authUser) {
+          const userStories = await StoryApi.getUserStories(authUser._id);
+          setUserStories(userStories);
+
+          const likedStories = await StoryApi.getLikedStories(authUser._id);
+          setLikedStories(likedStories);
+
+          const readStories = await StoryApi.getReadStories(authUser._id);
+          setReadStories(readStories);
+
+          const savedStories = await StoryApi.getSavedStories(authUser._id);
+          setSavedStories(savedStories);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
   }, [authUser]);
 
   return (
