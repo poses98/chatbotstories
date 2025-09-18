57605a7ced2378d97abe6d0d1b53d37d1f2153d0
Pablo Oses <44227608+poses98@users.noreply.github.com>
2025-09-18 17:28:32 +0200

    Clear Firebase config values for security
    Removed sensitive Firebase configuration details.

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCpitxa6of07zmFhubpG6AH1o9BcGWI4GQ',
-  authDomain: 'chatbotstories.firebaseapp.com',
-  projectId: 'chatbotstories',
-  storageBucket: 'chatbotstories.appspot.com',
-  messagingSenderId: '355055867626',
-  appId: '1:355055867626:web:b192912fd935da751be993',
-  measurementId: 'G-H9MZ3606L3',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 export const app =
